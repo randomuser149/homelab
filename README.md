4191e6fcdf59918fb5a20939e530d89e4604a372
randomuser149 <asdimasdi15@gmail.com>
2025-12-23 19:23:04 +0100

    Added files & updated README

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# homelab
-The configuration files and inner workings of my homelab setup
+# Google Cloud  
+This branch contains all files & commands that are and were used on the current Google Cloud VM instance.  
+It only runs Caddy and Vaultwarden as of currently.  
+Commands used can be found in the [commands.sh](./commands.sh)
+
+## The file structure on the instance:  
+```bash
+/  
+└── docker-compose/  
+    ├── compose.yml  
+    └── Caddyfile
+```
+
+## Instance configuration:  
+Name:    vaultwarden  
+Region:  us-central1  
+OS:      debian-12-bookworm  
+Machine type:  e2-micro (2 vCPUs, 1 GB memory)  
+HTTP traffic:  on  
+HTTPS traffic: on  
+Network tags:  http-server https-server server   
+Network interface name:  nic0  
+Network:                 mynetwork  
+Subnetwork:              subnet-us-central1  
+Network tier:            Standard  
+Storage: vaultwarden (Boot disk)  
+Size:    10 GB  
+Type:    Standard persistent disk  
+
+## VPC network configuration:  
+VPC network:  mynetwork  
+Subnets:      subnet-us-central1  
+Firewalls (all ingress, all allow):  
+- mynetwork-allow-http - tcp:80 ; target tags: http-server 
+- mynetwork-allow-https - tcp:443 ; target tags: https-server
+- mynetwork-allow-custom - tcp:22,80,443,8080,[ssh port] icmp ; target: Apply to all
+  
---
+++ commands.sh
@@ -0,0 +1,69 @@
+#################################################
+# Install docker (commands from docker docs)
+#################################################
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+##################################################
+# Create the dirs and copy the files
+##################################################
+
+sudo mkdir /docker-compose
+sudo curl -sL https://raw.githubusercontent.com/randomuser149/homelab/main/gcloud/docker-compose/docker-compose.yml -o /docker-compose/docker-compose.yml
+sudo curl -sL https://raw.githubusercontent.com/randomuser149/homelab/main/gcloud/docker-compose/Caddyfile -o /docker-compose/Caddyfile
+# replace [domain] & [email] with your own
+sudo vim /docker-compose/Caddyfile
+
+##################################################
+# Start the docker containers
+##################################################
+
+docker compose -f /docker-compose/docker-compose.yml up -d
+
+##################################################
+# Edit your firewalls
+##################################################
+
+sudo apt install ufw
+sudo ufw enable
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow [ssh port]/tcp
+sudo ufw reload
+
+##################################################
+# Move the default SSH port
+#
+# IMPORTANT: DO NOT CLOSE YOUR CURRENT SSH SESSION BEFORE YOU VALIDATED THE NEW ONE WORKS
+#
+# Note that if you follow through then you have to copy the private keys from your Cloud shell and save them to your local machine
+# Cloud Shell doesn't allow custom ssh ports, so that's why you have to 'abandon' it if you change the ports
+# The key is located at ~/.ssh/google_compute_engine
+# Download and move this key to C:\Users\[username]\.ssh\
+# Run these two commands before your first log in in PowerShell
+# icacls $env:USERPROFILE\.ssh\google_compute_engine /inheritance:r
+# icacls $env:USERPROFILE\.ssh\google_compute_engine /grant:r "$($env:USERNAME):(R)"
+# Use the following command to log in from PowerShell
+# ssh -i $env:USERPROFILE\.ssh\google_compute_engine -p [ssh port] [linux username]@[public ip]
+##################################################
+
+sudo vim /etc/ssh/sshd_config
+# Uncomment the Port 22 line, and replace 22 with your own choice.
+sudo systemctl restart sshd
